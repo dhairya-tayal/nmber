26be50442cb61623374e6b519304636c843599b3
dhairya-tayal <himanitayal1984@gmail.com>
2024-07-10 12:13:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add event listener to navigation links
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: 'mooth' });
+    });
+});
